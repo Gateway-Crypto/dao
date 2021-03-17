62e86b1590307d1540f5c143e7a633d860f7fe0e
Tim <tpkernell@gmail.com>
2021-03-16 19:00:51 -0500

    Fill in community DAO

--- contracts/Community.sol
+++ contracts/Community.sol
@@ -2,9 +2,15 @@
 
 pragma solidity 0.6.12;
 
+import "./CommunityToken.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract Community {
     
-    address public owner;
+    uint256 public proposalCount;
+    CommunityToken public communityToken;
+    uint256 public debatingPeriod;
+    uint256 public votingPeriod;
     
     enum Vote {
         Null,
@@ -13,6 +19,41 @@ contract Community {
     }
     
     struct Proposal {
+        address proposer;
+        string description;
+        uint256 startTime; // Timestamp when voting can start
+        mapping(address => Vote) votesByMember;
+    }
+    
+    mapping(uint256 => Proposal) public proposals;
+    mapping(address => bool) public members;
+    
+    modifier onlyShareholder {
+        require(communityToken.balanceOf(msg.sender) > 0);
+        _;
+    }
+    
+    modifier onlyMember {
+        require(members[msg.sender]);
+        _;
+    }
+    
+    constructor(
+        CommunityToken _communityToken,
+        uint256 _debatingPeriodInHours,
+        uint256 _votingPeriodInHours) 
+        public {
+        communityToken = _communityToken;
+        debatingPeriod = _debatingPeriodInHours * 1 hours;
+        votingPeriod = _votingPeriodInHours * 1 hours;
+    }
+    
+    function submitProposal(string memory _description) public onlyShareholder {
+        proposals[proposalCount] = Proposal(msg.sender, _description, now + debatingPeriod);
+        proposalCount++;
+    }
+    
+    function vote(uint256 proposalIndex, uint8 uintVote) public onlyShareholder onlyMember {
         
     }
 }
